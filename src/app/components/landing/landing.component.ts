caa0f11492624c464ad83bcf085db1d6a9b3a0a6
unknown <Sena CSET@BUCDFPCSEFSDXXX.FormacionSena.RED>
2024-04-30 14:51:51 -0500

    si

---
+++ src/app/components/landing/landing.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import {NgxTypedJsModule} from 'ngx-typed-js'
+
+@Component({
+  selector: 'app-landing',
+  standalone: true,
+  imports: [NgxTypedJsModule],
+  templateUrl: './landing.component.html',
+  styleUrl: './landing.component.css'
+})
+export class LandingComponent {
+
+}
